66fd3fc7eca282cce64870f43e2ebd24950af5c5
haeleth <haeleth@9001f46f-672a-0410-b4fc-d4ff93ad6438>
2008-04-16 20:08:45 +0000

    Changes to allow remote OS X build

--- osx_build.sh
+++ osx_build.sh
@@ -4,22 +4,23 @@
 
 make distclean &>/dev/null
 
-export LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.3.9.sdk -Wl,-framework,OpenGL"
+export EXTRALDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.3.9.sdk -Wl,-framework,OpenGL"
 export CCARGS="-arch ppc -isysroot /Developer/SDKs/MacOSX10.3.9.sdk"
 export CC="gcc $CCARGS"
 export CXX="g++ $CCARGS"
 export SDLOTHERCONFIG="--build=powerpc-apple-darwin7.9.0"
-export OTHERCONFIG="--build=powerpc-apple-darwin7.9.0"
+export OTHERCONFIG="--build=powerpc-apple-darwin7.9.0 --disable-sdltest"
 ./configure --with-internal-libs
 make || exit
 mv onscripter onscripter.ppc
 make distclean &>/dev/null
 
-export LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -Wl,-framework,OpenGL"
+export EXTRALDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -Wl,-framework,OpenGL"
 export CCARGS="-arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk"
 export CC="gcc $CCARGS"
 export CXX="g++ $CCARGS"
-export OTHERCONFIG="--build=i386-apple-darwin8.11.1"
+export SDLOTHERCONFIG="--build=i386-apple-darwin8.11.1"
+export OTHERCONFIG="--build=i386-apple-darwin8.11.1 --disable-sdltest"
 ./configure --with-internal-libs
 make || exit
 mv onscripter onscripter.intel
