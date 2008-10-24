34e84b023a952fb08a75a02cf3ced26524a3c100
sonozaki <sonozaki@9001f46f-672a-0410-b4fc-d4ff93ad6438>
2008-10-24 07:39:14 +0000

    changes to successfully build on mingw gcc 4.3.2

--- ONScripterLabel_command.cpp
+++ ONScripterLabel_command.cpp
@@ -2825,8 +2825,8 @@ int ONScripterLabel::exbtnCommand()
         sprite_no = script_h.readInt();
         no = script_h.readInt();
 
-        if (cellcheck_flag  && (sprite_info[ sprite_no ].num_of_cells < 2) ||
-            !cellcheck_flag && (sprite_info[ sprite_no ].num_of_cells == 0)){
+        if ((cellcheck_flag  && (sprite_info[ sprite_no ].num_of_cells < 2)) ||
+            (!cellcheck_flag && (sprite_info[ sprite_no ].num_of_cells == 0))){
             script_h.readStr();
             return RET_CONTINUE;
         }
@@ -3174,11 +3174,12 @@ int ONScripterLabel::cspCommand()
             si[i].remove();
         }
     else if (no >= 0 && no < MAX_SPRITE_NUM){
-        if ( si[no].visible )
+        if ( si[no].visible ) {
             if (csp2_flag)
                 dirty_rect.add( si[no].bounding_rect );
             else
                 dirty_rect.add( si[no].pos );
+        }
         if (!csp2_flag) root_button_link.removeSprite(no);
         si[no].remove();
     }
--- ONScripterLabel_text.cpp
+++ ONScripterLabel_text.cpp
@@ -31,15 +31,15 @@ extern unsigned short convSJIS2UTF16( unsigned short in );
 
 #define IS_ROTATION_REQUIRED(x)	\
         ( !IS_TWO_BYTE(*(x)) || \
-          *(x) == (char)0x81 && *((x)+1) == (char)0x50 || \
-          *(x) == (char)0x81 && *((x)+1) == (char)0x51 || \
-          *(x) == (char)0x81 && *((x)+1) >= 0x5b && *((x)+1) <= 0x5d || \
-          *(x) == (char)0x81 && *((x)+1) >= 0x60 && *((x)+1) <= 0x64 || \
-          *(x) == (char)0x81 && *((x)+1) >= 0x69 && *((x)+1) <= 0x7a || \
-          *(x) == (char)0x81 && *((x)+1) == (char)0x80 )
+          ((*(x) == (char)0x81) && (*((x)+1) == (char)0x50)) || \
+          ((*(x) == (char)0x81) && (*((x)+1) == (char)0x51)) || \
+          ((*(x) == (char)0x81) && (*((x)+1) >= 0x5b) && (*((x)+1) <= 0x5d)) || \
+          ((*(x) == (char)0x81) && (*((x)+1) >= 0x60) && (*((x)+1) <= 0x64)) || \
+          ((*(x) == (char)0x81) && (*((x)+1) >= 0x69) && (*((x)+1) <= 0x7a)) || \
+          ((*(x) == (char)0x81) && (*((x)+1) == (char)0x80)) )
 
 #define IS_TRANSLATION_REQUIRED(x)	\
-        ( *(x) == (char)0x81 && *((x)+1) >= 0x41 && *((x)+1) <= 0x44 )
+        ( (*(x) == (char)0x81) && (*((x)+1) >= 0x41) && (*((x)+1) <= 0x44) )
 
 SDL_Surface *ONScripterLabel::renderGlyph(TTF_Font *font, Uint16 text)
 {
@@ -100,7 +100,7 @@ void ONScripterLabel::drawGlyph( SDL_Surface *dst_surface, Fontinfo *info, SDL_C
     SDL_Surface *tmp_surface = renderGlyph( (TTF_Font*)info->ttf_font, unicode );
 
     bool rotate_flag = false;
-    if ( info->getTateyokoMode() == Fontinfo::TATE_MODE && IS_ROTATION_REQUIRED(text) ) rotate_flag = true;
+    if ( (info->getTateyokoMode() == Fontinfo::TATE_MODE) && IS_ROTATION_REQUIRED(text) ) rotate_flag = true;
 
     dst_rect.x = xy[0] + minx;
     dst_rect.y = xy[1] + TTF_FontAscent((TTF_Font*)info->ttf_font) - maxy;
@@ -270,7 +270,7 @@ void ONScripterLabel::drawString( const char *str, uchar3 color, Fontinfo *info,
             text[1] = *str++;
             drawChar( text, info, false, false, surface, cache_info );
         }
-        else if (*str == 0x0a || *str == '\\' && info->is_newline_accepted){
+        else if ((*str == 0x0a) || ((*str == '\\') && info->is_newline_accepted)){
             info->newLine();
             str++;
         }
@@ -595,11 +595,11 @@ int ONScripterLabel::textCommand()
     bool in_1byte_mode = false;
 
     if (pretextgosub_label && 
-        (!pagetag_flag || page_enter_status == 0) &&
-        (line_enter_status == 0 ||
-         (line_enter_status == 1 &&
-          (buf[0] == '[' ||
-           zenkakko_flag && buf[0] == (char)0x81 && buf[1] == (char)0x79))) ) {
+        (!pagetag_flag || (page_enter_status == 0)) &&
+        ((line_enter_status == 0) ||
+         ((line_enter_status == 1) &&
+          ((buf[0] == '[') ||
+           (zenkakko_flag && (buf[0] == (char)0x81) && (buf[1] == (char)0x79))))) ) {
         if (buf[0] == '[')
             buf++;
         else if (zenkakko_flag && buf[0] == (char)0x81 && buf[1] == (char)0x79)
