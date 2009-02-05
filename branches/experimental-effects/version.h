115b8fa6cc1bb0bcd4e7a386aef1fe8d8eacaeb0
sonozaki <sonozaki@9001f46f-672a-0410-b4fc-d4ff93ad6438>
2009-02-05 05:06:10 +0000

    fix for Mac skip-in-text lag, version

--- branches/experimental-effects/version.h
+++ branches/experimental-effects/version.h
@@ -1,6 +1,6 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
-#define VER_NUMBER 2009012-en-effects
+#define VER_NUMBER 20090122-en-effects
 #define ONS_VERSION xstr(VER_NUMBER)
 #define NSC_VERSION 281
